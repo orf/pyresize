2eb6dc604a1a156b75c6f0728ffbb087b41332fe
orf <tom@tomforb.es>
2017-02-06 00:53:30 +0000

    Small fixes

--- resize.py
+++ resize.py
@@ -2,9 +2,10 @@ import platform
 import os
 import sys
 
-def ResizeScreen(x,y):
-		clientos = platform.system()
-		if clientos == 'Windows':
-			os.system("mode {rows},{columns}".format(rows=x,columns=y))
-		elif clientos == 'Linux' or clientos == 'Darwin' or clientos.startswith('CYGWIN'):
-			sys.stdout.write("\x1b[8;{columns};{rows}t".format(rows=x,columns=y))
+clientos = platform.system()
+
+def resize_screen(x,y):
+    if clientos == 'Windows':
+	os.system("mode {rows},{columns}".format(rows=x,columns=y))
+    elif clientos in {'Linux', 'Darwin'} or clientos.startswith('CYGWIN'):
+	sys.stdout.write("\x1b[8;{columns};{rows}t".format(rows=x,columns=y))
